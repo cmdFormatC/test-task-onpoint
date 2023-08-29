f4891c84a01364f8e690146edeedc28512916f53
Bragin Fedor <bragin.fedor@gmail.com>
2023-08-29 16:49:11 +0400

    [~] add next button

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useRef } from "react";
 import "../index";
 import Header from "./Header";
 import Footer from "./Footer";
